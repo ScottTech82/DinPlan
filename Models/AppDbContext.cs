971595df16e9ad6af1721e088d16f713b9d1a8cc
Josh Scott <joshscott82@gmail.com>
2022-12-09 13:24:44 -0500

    init

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection.Emit;
+
+namespace DinnerPlanner.Models;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    public DbSet<Meal> Meals { get; set; }
+
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder) { }
+
+}
---
+++ Models/Meal.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DinnerPlanner.Models;
+
+public class Meal
+{
+    public int Id { get; set; }
+    [StringLength(30)]
+    public string Name { get; set; }
+    [StringLength(30)]
+    public string Type { get; set; }
+    [StringLength(255)]
+    public string? Description { get; set; }
+
+
+}
